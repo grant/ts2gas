8770b384b93c5abd8746c88255689c9f96841ff4
Grant Timmerman <timmerman+devrel@google.com>
2018-07-31 19:14:39 -0700

    Transpile modules and imports appropriately

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ var ts = require("typescript");
  * @see https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
  */
 module.exports = (source) => {
+  // Before transpiling, apply these touch-ups:
+  
+  // ## Imports
+  // Some editors (like IntelliJ) automatically import identifiers.
+  // Individual imports lines are commented out
+  // i.e. import GmailMessage = GoogleAppsScript.Gmail.GmailMessage;
+  source = source.replace(/^.*import /mg, '// import ');
+
+  // Transpile
   // https://www.typescriptlang.org/docs/handbook/compiler-options.html
-  var result = ts.transpileModule(source, {
+  let result = ts.transpileModule(source, {
     compilerOptions: {
       lib: 'ES2015',
       target: 'ES3',
@@ -18,8 +27,30 @@ module.exports = (source) => {
       noResolve: true,
       pretty: true,
       module: ts.ModuleKind.None,
-      // moduleResolution: false,
+      moduleResolution: false,
     }
   });
-  return result.outputText;
+
+  // After transpiling, apply these touch-ups:
+
+  // # Clean up output (multiline string)
+  let output = result.outputText;
+  
+  // ## exports.__esModule
+  // Remove all lines that have exports.__esModule = true
+  // @see https://github.com/Microsoft/TypeScript/issues/14351
+  output = output.replace('exports.__esModule = true;', ''); // Remove this line
+
+  // ## Exports
+  // Exports are transpiled to variables 'exports' and 'module.exports'
+
+  // Include an exports object in all files.
+  output = `var exports = exports || {};
+var module = module || { exports: exports };\n` + output;
+
+  // Remove default exports
+  // (Transpiled `exports["default"]`)
+  output = output.replace(/^.*exports\[\"default\"\].*$\n/mg, '');
+
+  return output;
 }
--- test.js
+++ test.js
@@ -8,29 +8,80 @@ const ts2gas = require('./');
 function printBeforeAndAfter(code) {
   console.log('v--TS--v');
   console.log(code);
-  console.log('--');
+  console.log('–––');
   console.log(ts2gas(code), '^--GS--^'); // Prevents newline
-  console.log();
 }
 
-console.log('## TESTS ##')
-// Test a simple conversion.
-printBeforeAndAfter('const hi:string = `Hi ${1 + 2}`;');
-printBeforeAndAfter(`class Hamburger {
+const tests = {
+  testConst: () => {
+    printBeforeAndAfter('const hi:string = `Hi ${1 + 2}`;');
+  },
+  testClass: () => {
+    printBeforeAndAfter(
+`class Hamburger {
   constructor() {
     // This is the constructor.
   }
   listToppings() {
     // This is a method.
   }
-}`);
-printBeforeAndAfter(`
+}`
+    );
+  },
+  testTemplateStrings: () => {
+    printBeforeAndAfter(
+`var name = 'Grant';
+var age = 42;
+console.log(\`Hello! My name is \${name}, and I am not \${age} years old.\`);
+`);
+  },
+  testArraySpread: () => {
+    printBeforeAndAfter(
+`let cde = ['c', 'd', 'e'];
+let scale = ['a', 'b', ...cde, 'f', 'g'];`);
+  },
+  testDestructure: () => {
+    printBeforeAndAfter(
+`let jane = { firstName: 'Jane', lastName: 'Doe'};
+let john = { firstName: 'John', lastName: 'Doe', middleName: 'Smith' }
+function sayName({firstName, lastName, middleName = 'N/A'}) {
+  console.log(\`Hello \${firstName} \${middleName} \${lastName}\`)
+}
+sayName(jane) // -> Hello Jane N/A Doe
+sayName(john) // -> Helo John Smith Doe`);
+  },
+  testImport: () => {
+    printBeforeAndAfter(
+`import ContentAlignment = GoogleAppsScript.Slides.ContentAlignment;
 import GmailMessage = GoogleAppsScript.Gmail.GmailMessage;
 
 function getCurrentMessage():GmailMessage {
-  return GmailApp.createDraft("", "", "").send()
-}
+  return GmailApp.createDraft("", "", "").send();
+}`);
+  },
+  testExport: () => {
+    printBeforeAndAfter(`export const pi = 3.141592;`);
+  },
+  testExportDefault: () => {
+    printBeforeAndAfter(`export default const pi = 3.141592;`);
+  },
+  testModuleExports: () => {
+    printBeforeAndAfter(`module.exports = 3.14;`);
+  },
+  testModuleExportsObject: () => {
+    printBeforeAndAfter(`module.exports.foo = {};`);
+  },
+  testRequire: () => {
+    printBeforeAndAfter(`const a = require('foo');`);
+  }
+};
 
-getCurrentMessage();
-`);
-// TODO: Add more examples
+// Run tests
+console.log('## TESTS ##')
+const testNames = Object.keys(tests);
+console.log('---------------------------------------------------------------------------------');
+for (let testName of testNames) {
+  console.log('# ' + testName);
+  tests[testName]();
+  console.log('---------------------------------------------------------------------------------');
+}
