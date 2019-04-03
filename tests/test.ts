88d17bd874ab04494b77f1c59aa4a706c67eb437
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-04-03 10:20:15 +0200

    Commented out parts which crashes ts.transpileModule

--- tests/test.ts
+++ tests/test.ts
@@ -217,13 +217,13 @@ const flipped = flip(zip);  // <T, U>(b: U, a: T) => [T, U]
 const t1 = flipped(10, 'hello');  // [string, number]
 const t2 = flipped(true, 0);  // [number, boolean]`);
   },
-//   testTypeScript_34x_improvedReadonly: () => {
-//     printBeforeAndAfter(
-// `// Improved support for read-only arrays and tuples
-// function f1(mt: [number, number], rt: readonly [number, number]) {
-//   mt[0] = 1;  // Ok
-//   rt[0] = 1;  // Error, read-only element
-// }
+  testTypeScript_34x_improvedReadonly: () => {
+    printBeforeAndAfter(
+`// Improved support for read-only arrays and tuples
+function f1(mt: [number, number], rt: readonly [number, number]) {
+  mt[0] = 1;  // Ok
+  // rt[0] = 1;  // Error, read-only element
+}
 
 // function f2(ma: string[], ra: readonly string[], mt: [string, string], rt: readonly [string, string]) {
 //   ma = ra;  // Error
@@ -240,14 +240,14 @@ const t2 = flipped(true, 0);  // [number, boolean]`);
 //   rt = mt;  // Ok
 // }
 
-// type ReadWrite<T> = { -readonly [P in keyof T] : T[P] };
+type ReadWrite<T> = { -readonly [P in keyof T] : T[P] };
 
-// type T0 = Readonly<string[]>;  // readonly string[]
-// type T1 = Readonly<[number, number]>;  // readonly [number, number]
-// type T2 = Partial<Readonly<string[]>>;  // readonly (string | undefined)[]
-// type T3 = Readonly<Partial<string[]>>;  // readonly (string | undefined)[]
-// type T4 = ReadWrite<Required<T3>>;  // string[]`);
-//   },
+type T0 = Readonly<string[]>;  // readonly string[]
+type T1 = Readonly<[number, number]>;  // readonly [number, number]
+type T2 = Partial<Readonly<string[]>>;  // readonly (string | undefined)[]
+type T3 = Readonly<Partial<string[]>>;  // readonly (string | undefined)[]
+type T4 = ReadWrite<Required<T3>>;  // string[]`);
+  },
   testTypeScript_34x_constContext: () => {
     printBeforeAndAfter(
 `// Const contexts for literal expressions
