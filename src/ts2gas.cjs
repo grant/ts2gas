c108b579c40b0d9a158d5b43462388b3f9602ccb
symbl-ay <andrew.yurisich@symbl.ai>
2022-06-09 14:11:46 -0400

    fix(cjs): Expose require-able interface for commonjs

--- src/ts2gas.cjs
+++ src/ts2gas.cjs
@@ -1,5 +1,7 @@
-if (require.main === module) {
-  import('./index.js').then(index => {
+import('./index.js').then(index => {
+  if (require.main === module) {
     process.stdout.write(index.default(process.argv[2]));
-  });
-}
+  } else {
+    module.exports.default = index.default;
+  }
+});
